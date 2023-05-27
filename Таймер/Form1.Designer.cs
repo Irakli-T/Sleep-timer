b27ac17ff37552152b551678eba4a9c174417c41
irakli tomaradze <irakli.tomaradze2014@yandex.ru>
2023-05-27 10:29:40 +0400

    Добавьте файлы проекта.

---
+++ Таймер/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Таймер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHour = new System.Windows.Forms.TextBox();
+            this.textBoxMinutes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonSoundChanges = new System.Windows.Forms.Button();
+            this.buttonMonitorShutdown = new System.Windows.Forms.Button();
+            this.textBoxSeconds = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Label();
+            this.ButtonWelding = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(104, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выключение произойдет через";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxHour
+            // 
+            this.textBoxHour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(21)))));
+            this.textBoxHour.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxHour.ForeColor = System.Drawing.Color.Goldenrod;
+            this.textBoxHour.Location = new System.Drawing.Point(160, 107);
+            this.textBoxHour.Name = "textBoxHour";
+            this.textBoxHour.Size = new System.Drawing.Size(35, 31);
+            this.textBoxHour.TabIndex = 1;
+            this.textBoxHour.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxHour_KeyPress);
+            // 
+            // textBoxMinutes
+            // 
+            this.textBoxMinutes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(21)))));
+            this.textBoxMinutes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMinutes.ForeColor = System.Drawing.Color.Goldenrod;
+            this.textBoxMinutes.Location = new System.Drawing.Point(225, 107);
+            this.textBoxMinutes.Name = "textBoxMinutes";
+            this.textBoxMinutes.Size = new System.Drawing.Size(35, 31);
+            this.textBoxMinutes.TabIndex = 2;
+            this.textBoxMinutes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMinutes_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label3.Location = new System.Drawing.Point(191, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ч";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label4.Location = new System.Drawing.Point(256, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "М";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.Transparent;
+            this.buttonStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonStart.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.buttonStart.FlatAppearance.BorderSize = 3;
+            this.buttonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Goldenrod;
+            this.buttonStart.Location = new System.Drawing.Point(191, 167);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 33);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Запустить";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSoundChanges
+            // 
+            this.buttonSoundChanges.BackColor = System.Drawing.Color.Transparent;
+            this.buttonSoundChanges.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSoundChanges.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.buttonSoundChanges.FlatAppearance.BorderSize = 2;
+            this.buttonSoundChanges.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonSoundChanges.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonSoundChanges.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSoundChanges.ForeColor = System.Drawing.Color.Goldenrod;
+            this.buttonSoundChanges.Location = new System.Drawing.Point(80, 228);
+            this.buttonSoundChanges.Name = "buttonSoundChanges";
+            this.buttonSoundChanges.Size = new System.Drawing.Size(145, 25);
+            this.buttonSoundChanges.TabIndex = 6;
+            this.buttonSoundChanges.Text = "Вкл. Снижение звука";
+            this.buttonSoundChanges.UseVisualStyleBackColor = false;
+            this.buttonSoundChanges.Click += new System.EventHandler(this.buttonSoundChanges_Click);
+            // 
+            // buttonMonitorShutdown
+            // 
+            this.buttonMonitorShutdown.BackColor = System.Drawing.Color.Transparent;
+            this.buttonMonitorShutdown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonMonitorShutdown.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.buttonMonitorShutdown.FlatAppearance.BorderSize = 2;
+            this.buttonMonitorShutdown.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonMonitorShutdown.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonMonitorShutdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMonitorShutdown.ForeColor = System.Drawing.Color.Goldenrod;
+            this.buttonMonitorShutdown.Location = new System.Drawing.Point(255, 228);
+            this.buttonMonitorShutdown.Name = "buttonMonitorShutdown";
+            this.buttonMonitorShutdown.Size = new System.Drawing.Size(145, 25);
+            this.buttonMonitorShutdown.TabIndex = 7;
+            this.buttonMonitorShutdown.Text = "Выключение монитора";
+            this.buttonMonitorShutdown.UseVisualStyleBackColor = false;
+            this.buttonMonitorShutdown.Click += new System.EventHandler(this.buttonMonitorShutdown_Click);
+            // 
+            // textBoxSeconds
+            // 
+            this.textBoxSeconds.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(21)))));
+            this.textBoxSeconds.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSeconds.ForeColor = System.Drawing.Color.Goldenrod;
+            this.textBoxSeconds.Location = new System.Drawing.Point(289, 107);
+            this.textBoxSeconds.Name = "textBoxSeconds";
+            this.textBoxSeconds.Size = new System.Drawing.Size(35, 31);
+            this.textBoxSeconds.TabIndex = 8;
+            this.textBoxSeconds.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSeconds_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label5.Location = new System.Drawing.Point(320, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "С";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.BackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseButton.ForeColor = System.Drawing.Color.Goldenrod;
+            this.CloseButton.Location = new System.Drawing.Point(460, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(24, 24);
+            this.CloseButton.TabIndex = 10;
+            this.CloseButton.Text = "X";
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.CloseButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            this.CloseButton.MouseLeave += new System.EventHandler(this.CloseButton_MouseLeave);
+            // 
+            // ButtonWelding
+            // 
+            this.ButtonWelding.AutoSize = true;
+            this.ButtonWelding.BackColor = System.Drawing.Color.Transparent;
+            this.ButtonWelding.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonWelding.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonWelding.ForeColor = System.Drawing.Color.Goldenrod;
+            this.ButtonWelding.Location = new System.Drawing.Point(439, -8);
+            this.ButtonWelding.Name = "ButtonWelding";
+            this.ButtonWelding.Size = new System.Drawing.Size(26, 29);
+            this.ButtonWelding.TabIndex = 11;
+            this.ButtonWelding.Text = "_";
+            this.ButtonWelding.Click += new System.EventHandler(this.ButtonWelding_Click);
+            this.ButtonWelding.MouseEnter += new System.EventHandler(this.ButtonWelding_MouseEnter);
+            this.ButtonWelding.MouseLeave += new System.EventHandler(this.ButtonWelding_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Таймер.Properties.Resources.Фон_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 303);
+            this.Controls.Add(this.ButtonWelding);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxSeconds);
+            this.Controls.Add(this.buttonMonitorShutdown);
+            this.Controls.Add(this.buttonSoundChanges);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMinutes);
+            this.Controls.Add(this.textBoxHour);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Таймер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxHour;
+        private System.Windows.Forms.TextBox textBoxMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonSoundChanges;
+        private System.Windows.Forms.Button buttonMonitorShutdown;
+        private System.Windows.Forms.TextBox textBoxSeconds;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label CloseButton;
+        private System.Windows.Forms.Label ButtonWelding;
+    }
+}
+
---
+++ Таймер/Form1.cs
@@ -0,0 +1,307 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace Таймер
+{
+    public partial class Form1 : Form
+    {
+        // Константы, необходимые для управления монитором
+        private const int SC_MONITORPOWER = 0xF170;
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int MONITOR_OFF = 2;
+        
+        [DllImport("user32.dll")]  // Импортируем функцию SendNotifyMessage из user32.dll
+        private static extern int SendNotifyMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+       
+        private string Hour = "";
+        private string Minutes = "";
+        private string Seconds = "";
+        private bool timerActive = false; // Флаг, указывающий, активен ли таймер
+        private bool sleepMode = false; // Флаг, указывающий, находится ли компьютер в спящем режиме
+        [DllImport("powrprof.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+        private bool soundChangesEnabled = false; // Добавляем переменную для отслеживания состояния функции
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval =60000;
+            timer1.Tick += new EventHandler(Timer1_Tick);
+            PowerModeChangedEventHandler SystemEvents_PowerModeChanged = null;
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged; // Подписываемся на событие изменения режима энергосбережения
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hour = "0";
+            Minutes = "0";
+            Seconds = "0"; ;
+
+            if (Hour.Length == 1)
+            {
+                Hour = "0" + Hour;
+            }
+            if (Minutes.Length == 1)
+            {
+                Minutes = "0" + Minutes;
+            }
+            if (Seconds.Length == 1)
+            {
+                Seconds = "0" + Seconds;
+            }
+
+            textBoxHour.Text = Hour;
+            textBoxMinutes.Text = Minutes;
+            textBoxSeconds.Text = Seconds;
+        }
+
+        private void textBoxHour_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if (textBoxHour.Text.Length >= 0 && textBoxHour.Text.Length <= 1)
+                {
+                    return;
+                }
+
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (Char)Keys.Back)
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
+        }
+        private void textBoxMinutes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if (textBoxMinutes.Text.Length >= 0 && textBoxMinutes.Text.Length <= 1)
+                {
+                    return;
+                }
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (Char)Keys.Back)
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
+        }
+        private void textBoxSeconds_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if (textBoxSeconds.Text.Length >= 0 && textBoxSeconds.Text.Length <= 1)
+                {
+                    return;
+                }
+
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (Char)Keys.Back)
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStart.Text == "Запустить")
+            {
+                if (textBoxSeconds.Text == "" || Convert.ToInt32(textBoxSeconds.Text) > 59)
+                {
+                    MessageBox.Show("Не введены секунды!");
+                    return;
+                }
+                if (textBoxMinutes.Text == "" || Convert.ToInt32(textBoxMinutes.Text) > 59)
+                {
+                    MessageBox.Show("Не введены минуты!");
+                    return;
+                }
+                if (textBoxHour.Text == "")
+                {
+                    MessageBox.Show("Не введены часы!");
+                    return;
+                }
+
+                timer1.Stop();
+                timer1.Interval = 1000;
+                timer1.Start();
+
+                timer1.Enabled = true;
+                //buttonStart.BackColor = Color.FromArgb(56, 55, 52);
+               // buttonStart.ForeColor = Color.Black; // Добавленный код для изменения цвета текста на черный
+                buttonStart.Text = "Стоп";
+                textBoxHour.ReadOnly = true;
+                textBoxMinutes.ReadOnly = true;
+                textBoxSeconds.ReadOnly = true;
+                timerActive = true;
+            }
+            else
+            {
+                
+                timer1.Enabled = false;
+                buttonStart.BackColor = Color.Transparent;
+                buttonStart.Text = "Запустить";
+                textBoxHour.ReadOnly = false;
+                textBoxMinutes.ReadOnly = false;
+                textBoxSeconds.ReadOnly = false;
+                timerActive = false;
+            }
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e) //обработчик событий изменения режима энергосбережения 
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Suspend:
+                    sleepMode = true;
+                    break;
+                case PowerModes.Resume:
+                    sleepMode = false;
+                    break;
+            }
+        }
+        
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            int hours = Convert.ToInt32(textBoxHour.Text);
+            int minutes = Convert.ToInt32(textBoxMinutes.Text);
+            int seconds = Convert.ToInt32(textBoxSeconds.Text);
+
+            if (seconds != 0)
+            {
+                seconds--;
+            }
+            else
+            {
+                if (minutes != 0)
+                {
+                    minutes--;
+                    seconds = 59;
+                }
+                else
+                {
+                    if (hours != 0)
+                    {
+                        hours--;
+                        minutes = 59;
+                        seconds = 59;
+                    }
+                    else
+                    {
+                        if (!sleepMode) // проверяем, не находится ли компьютер в спящем режиме
+                        {
+                            SetSuspendState(false, true, true); // переводим компьютер в спящий режим
+                        }
+                    }
+                }
+            }
+            textBoxHour.Text = hours.ToString("00");
+            textBoxMinutes.Text = minutes.ToString("00");
+            textBoxSeconds.Text = seconds.ToString("00");
+        }
+
+        private async void buttonSoundChanges_Click(object sender, EventArgs e)
+        {
+            // Создаем объект управления аудиоустройствами
+            MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+            // Получаем устройство вывода по умолчанию
+            MMDevice defaultDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            // Получаем текущее значение громкости в процентах
+            int currentVolume = (int)(defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+           
+            // Расчет задержки изменения громкости, относительно времени
+            int hours = Int32.Parse(textBoxHour.Text);
+            int minutes = Int32.Parse(textBoxMinutes.Text);
+            int totalMinutes = hours * 60 + minutes;
+            double delaySeconds = (double)totalMinutes / currentVolume * 60;
+            if (soundChangesEnabled)
+            {
+                // Если функция уже включена, то выключаем ее
+                buttonSoundChanges.Text = "Вкл. Снижение звука";
+                soundChangesEnabled = false;
+                return; // Выходим из функции
+            }
+
+            // Включаем функцию и меняем текст кнопки
+            buttonSoundChanges.Text = "Выкл. Снижение звука";
+            soundChangesEnabled = true;
+
+            for (int i = currentVolume; i >= 0; i--)
+            {
+                if (!soundChangesEnabled)
+                {
+                    break; // Если функция была выключена во время выполнения, выходим из цикла
+                }
+                float newVolume = (float)i / 100;
+                defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume; // установка значения громкости
+                await Task.Delay(TimeSpan.FromSeconds(delaySeconds)); // добавляем задержку, чтобы изменения громкости были заметны
+            }
+        }
+       
+        private void buttonMonitorShutdown_Click(object sender, EventArgs e)
+        {
+            // Отправляем сообщение SC_MONITORPOWER с параметром MONITOR_OFF
+             SendNotifyMessage((IntPtr)0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
+             buttonMonitorShutdown.Enabled = false;
+            // Ваш код здесь
+            buttonMonitorShutdown.Enabled = true;
+        }
+
+        Point lastPoint;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.FromArgb(218, 13, 29);
+        }
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.Goldenrod;
+        }
+
+        private void ButtonWelding_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void ButtonWelding_MouseEnter(object sender, EventArgs e)
+        {
+            ButtonWelding.ForeColor = Color.FromArgb(218, 13, 29);
+        }
+        private void ButtonWelding_MouseLeave(object sender, EventArgs e)
+        {
+            ButtonWelding.ForeColor = Color.Goldenrod;
+        }
+    }
+}
+
